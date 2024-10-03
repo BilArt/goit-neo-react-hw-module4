7fda6c1c7b9bddde21fc87c36bc3efad3984f75c
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-10-03 17:45:47 +0200

    done ImageModal

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,37 +1,24 @@
-import { useEffect } from 'react';
+import Modal from 'react-modal';
 import PropTypes from 'prop-types';
 import styles from './ImageModal.module.css';
 
+Modal.setAppElement('#root');
+
 const ImageModal = ({ isOpen, onRequestClose, image }) => {
-
-  console.log('Modal is open:', isOpen);
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === 'Escape') {
-        onRequestClose();
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [onRequestClose]);
-
-  if (!isOpen) return null;
-
   return (
-    <div className={`${styles.modalOverlay} ${isOpen ? styles.active : ''}`} onClick={onRequestClose}>
-      <div className={`${styles.modalContent} ${isOpen ? styles.active : ''}`} onClick={(e) => e.stopPropagation()}>
-        <img src={image.urls.regular} alt={image.alt_description} />
-        <div>
-          <h2>{image.user.name}</h2>
-          <p>Likes: {image.likes}</p>
-        </div>
-        <button onClick={onRequestClose}>Close</button>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className={styles.modalContent}
+      overlayClassName={styles.modalOverlay}
+    >
+      <img src={image.urls.regular} alt={image.alt_description} />
+      <div>
+        <h2>{image.user.name}</h2>
+        <p>Likes: {image.likes}</p>
       </div>
-    </div>
+      <button onClick={onRequestClose}>Close</button>
+    </Modal>
   );
 };
 
